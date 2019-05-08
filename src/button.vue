4c05b69dc3954104014ea9196fd682097dae01ef
“WineWang0000” <1205294873@qq.com>
2019-05-08 20:29:35 +0800

    添加 icon和iconPosition属性

--- src/button.vue
+++ src/button.vue
@@ -1,14 +1,40 @@
 <template>
-  <button class="g-button">按钮</button>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
+      <svg v-if="icon" class="icon">
+        <use :xlink:href=`#icon-${icon}`>
+        </use>
+      </svg>
+      <div class="content">
+        <slot></slot>
+      </div>
+    </button>
 </template>
 <script>
-  export default{}
+  export default{
+    props:{
+      icon:{},
+      iconPosition:{
+        type: String,
+        default: 'left'
+      }
+    }
+  }
 </script>
 <style lang="scss">
   .g-button{
+    height:var(--height);
     font-size: var(--font-size);
     padding: 0 1em;
     border-radius:var(--button-border-radius);
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    &:hover{border-color:var(--border-color-hover);}
+    > .icon{order:1;}
+    > .content{order: 2;}
+    &.icon-right{
+      > .icon{order:2;}
+      > .content{order:1;}
+    }
   }
-  .g-button:hover{border-color:var(--border-color-hover);}
 </style>
