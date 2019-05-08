f0a6c08927d7363d81a8f23ce0471ece1deecd60
“WineWang0000” <1205294873@qq.com>
2019-05-09 00:28:09 +0800

    用chai单元测试： click （nmp chai-spies）

--- src/app.js
+++ src/app.js
@@ -14,6 +14,8 @@ new Vue({
   }
 })
 import chai from 'chai'
+import spies from 'chai-spies'
+chai.use(spies)
 const expect = chai.expect
 {
   const Constructor = Vue.extend(Button)
@@ -74,4 +76,18 @@ const expect = chai.expect
   expect(order).to.eq('2')
   button.$el.remove()
   button.$destroy()
+}
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData:{
+      icon:'setting',
+    }
+  })
+  vm.$mount()
+  let spy = chai.spy(function(){})
+  vm.$on('click', spy)
+  let button = vm.$el
+  button.click()
+  expect(spy).to.have.been.called()
 }
