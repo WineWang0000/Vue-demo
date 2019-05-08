c8284816ed0df4d86128ad4ad6f33c85569dc858
“WineWang0000” <1205294873@qq.com>
2019-05-08 20:30:51 +0800

    验证iconPosition 只能为left或right

--- src/button.vue
+++ src/button.vue
@@ -15,7 +15,10 @@
       icon:{},
       iconPosition:{
         type: String,
-        default: 'left'
+        default: 'left',
+        validator(value){
+          return value === 'left' || value ==='right'
+        }
       }
     }
   }
