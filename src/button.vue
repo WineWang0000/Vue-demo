267857104f0dc3b747fca80fe8cdbc34ea5c1da7
“WineWang0000” <1205294873@qq.com>
2019-05-08 15:58:52 +0800

    mkdir src

---
+++ src/button.vue
@@ -0,0 +1,14 @@
+<template>
+  <button class="g-button">按钮</button>
+</template>
+<script>
+  export default{}
+</script>
+<style lang="scss">
+  .g-button{
+    font-size: var(--font-size);
+    padding: 0 1em;
+    border-radius:var(--button-border-radius);
+  }
+  .g-button:hover{border-color:var(--border-color-hover);}
+</style>
