41d5cce1fdb9a024f538d97c76b111d0c25599a3
“WineWang0000” <1205294873@qq.com>
2019-05-11 23:07:00 +0800

    栅格系统

---
+++ src/col.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]"
+  :style="{paddingLeft: gutter/2 + 'px', paddingRight: gutter/2 + 'px'}">
+    <div style="border: 1px solid green;height: 100px;"></div>
+    <slot></slot>
+  </div>
+</template>
+<script>
+  export default{
+    props: {
+      span: {
+        type: [String, Number]
+      },
+      offset: {
+        type: [String, Number]
+      },
+    },
+     data(){
+      return {
+        gutter:0
+      }
+     },
+    created(){},
+    mounted(){},
+  }
+</script>
+<style lang="scss" scoped>
+  .col{
+    padding: 5px 10px;
+    height: 100px;
+    width: 50%;
+    $class: col-;
+    @for $n from 1 through 24{
+      &.#{$class}#{$n}{
+        width: ($n / 24) * 100%;
+      }
+    }
+    $class: offset-;
+    @for $n from 1 through 24{
+      &.#{$class}#{$n}{
+        margin-left: ($n / 24) * 100%;
+      }
+    }
+  }
+</style>
---
+++ src/row.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="row" :style="{marginLeft: -gutter/2 + 'px', marginRight: -gutter/2 + 'px'}">
+    <slot></slot>
+  </div>
+</template>
+<script>
+  export default{
+    props:{
+      gutter:{
+        type: [String, Number]
+      }
+    },
+    created(){
+      console.log(this.$children)
+    },
+    mounted(){
+      console.log(this.$children)
+      this.$children.forEach((vm)=>{
+        vm.gutter = this.gutter
+      })
+    }
+  }
+</script>
+<style lang="scss" scoped>
+  .row{
+    display: flex;
+  }
+</style>
