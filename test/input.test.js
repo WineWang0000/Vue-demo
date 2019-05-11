950c710cbd45eec1303749c391c7090d5b72886d
“WineWang0000” <1205294873@qq.com>
2019-05-11 11:06:25 +0800

    添加 afterEach(()=>{})用法

--- test/input.test.js
+++ test/input.test.js
@@ -9,20 +9,22 @@ describe('Input', () => {
   it('存在.', () => {
     expect(Input).to.exist
   })
-  it('props', ()=>{
+  describe('props', ()=>{
+    const Constructor = Vue.extend(Input)
+    let vm
+    afterEach(()=>{
+      vm.$destroy()
+    })
     it('接收 value',() =>{
-      const Constructor = Vue.extend(Input)
-      const vm = new Constructor({
+      vm = new Constructor({
         propsData:{
           value: '123'
         }
       }).$mount()
       const inputEle = vm.$el.querySelector('input')
       expect(inputEle.value).to.eq('123')
-      vm.$destroy()
     })
     it('接收 disabled',() =>{
-      const Constructor = Vue.extend(Input)
       const vm = new Constructor({
         propsData:{
           disabled: true
@@ -30,22 +32,18 @@ describe('Input', () => {
       }).$mount()
       const inputEle = vm.$el.querySelector('input')
       expect(inputEle.disabled).to.eq(true)
-      vm.$destroy()
     })
     it('接收 readonly',() =>{
-      const Constructor = Vue.extend(Input)
-      const vm = new Constructor({
+      vm = new Constructor({
         propsData:{
           readonly: true
         }
       }).$mount()
       const inputEle = vm.$el.querySelector('input')
       expect(inputEle.readOnly).to.eq(true)
-      vm.$destroy()
     })
     it('接收 error', ()=>{
-      const Constructor = Vue.extend(Input)
-      const vm = new Constructor({
+      vm = new Constructor({
         propsData:{
           error: '你错了'
         }
@@ -56,5 +54,4 @@ describe('Input', () => {
       expect(errorMessage.innerText).to.eq('你错了')
     })
   })
-
 })
