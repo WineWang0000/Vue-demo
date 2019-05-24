f5e624a6c0edc445b8cc3c123cfbed9dd3d6ae89
“WineWang0000” <1205294873@qq.com>
2019-05-24 10:26:11 +0800

    添加 single 选项

--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="collapseItem">
-    <div class="title" @click="open=!open">
+    <div class="title" @click="toggle">
       {{title}}
     </div>
-    <div class="content">
+    <div class="content" v-if="open">
       <slot></slot>
     </div>
   </div>
@@ -16,13 +16,33 @@
       title:{
         type: String,
         required: true
-      }
+      },
     },
     data:{
      open: false
+    },
+    inject:['eventBus'],
+    mounted(){
+      this.eventBus && this.eventBus.$on('update: selected', (vm)=>{
+        if(vm!== this){
+          this.close()
+        }
+      })
+    },
+    methods:{
+      toggle(){
+        if(this.open){
+          this.open = false
+        }else{
+          this.open = true
+          this.eventBus && this.eventBus.$emit('update: selected', this)
+        }
+      },
+      close(){
+        this.open = false
+      }
     }
   }
-  console.log(1);
 </script>
 
 <style lang="scss">
--- src/collapse.vue
+++ src/collapse.vue
@@ -7,6 +7,24 @@
   import Vue from 'vue'
   export default{
     name: "GuluCollapse",
+    data(){
+      return{
+        eventBus: new Vue()
+      }
+    },
+    props:{
+      single:{
+        type: Boolean,
+        default: false
+      }
+    },
+    provide(){
+      if(this.single){
+        return{
+          eventBus: this.eventBus
+        }
+      }
+    }
   }
   console.log(2);
 </script>
